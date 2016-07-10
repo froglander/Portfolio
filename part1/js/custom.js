8fb5d5bf0e956a666433b69c126896ef613fd429
Kristi Heredia <froglander@dukworld.net>
2016-07-09 21:14:56 -0400

    Worked on custom.js and style.css

--- part1/js/custom.js
+++ part1/js/custom.js
@@ -13,6 +13,7 @@
         $('a[href*=#]:not([href=#])').click(function() {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
                 var target = $(this.hash);
+                // Added this to get a value set in the html file to use as a buffer for how far to scroll to leave part of the header image showing
                 var buffer = $(this).data("buffer");
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
@@ -39,6 +40,10 @@
                 });
                 // Displays secondary page heading once you scroll down
                 $("#secondaryTitle").show();
+
+                // Adds a background to menu links so they are visible
+                $(".navbar-custom").css({ background: 'rgba(0, 0, 0, 0.30)'});
+                $(".navbar-nav > li > a").css({ color: '#eee'});
             }
         } else {
             if (fixed) {
@@ -48,11 +53,15 @@
                         display: 'none'
                     });
                 });
+                // Transition the secondaryTitle off the screen when you scroll back up
                 $('#secondaryTitle').hide("slow", function() {
                     $('#secondaryTitle').css({
-                        display: 'none'                        
+                        display: 'none'
                     });                    
                 });  
+                // Set the navbar back to transparent when you scroll back up
+                $(".navbar-custom").css({ background: 'transparent'});
+                $(".navbar-nav > li > a").css({ color: '#777'});
             }
         }
     });
@@ -76,3 +85,27 @@
         }
         // Enable map zooming with mouse scroll when the user clicks the map
     $('.map').on('click', onMapClickHandler);
+
+    
+    // Added this function to calculate the position for the secondary title to display when you click the button
+    // under the h1 title
+    $(document).ready(function() {
+        adjustTop();
+        $(window).on("resize", adjustTop);
+    })
+
+    function adjustTop() {        
+        var buffer = $("#hdrButton").data("buffer");
+        var secondaryBuffer = (buffer - $("#secondaryTitle").height()/2 ) / 2;
+        var offsetTop = $("#h1Name").offset().top;
+        // var headerHeight = 69.5;
+
+        if (offsetTop < 200) {
+            offsetTop = 200;
+        }
+        
+        var headerHeight = offsetTop - buffer + secondaryBuffer;            
+        
+
+        $("#secondaryTitle").css("top", headerHeight);
+    }
